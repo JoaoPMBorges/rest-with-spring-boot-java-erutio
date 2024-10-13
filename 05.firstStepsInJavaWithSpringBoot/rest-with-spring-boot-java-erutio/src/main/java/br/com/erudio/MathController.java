3432b0ae39b80f9fd07397adc32331aa93af6407
Joao Pedro <jpmbofc@gmail.com>
2024-10-12 21:56:06 -0300

    [ParamentersAndExceptions]

---
+++ 05.firstStepsInJavaWithSpringBoot/rest-with-spring-boot-java-erutio/src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,41 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value ="/sum/{numberOne}/{numberTwo}",
+			method=RequestMethod.GET)
+	public Double sum(
+	  @PathVariable(value = "numberOne") String numberOne,
+	  @PathVariable(value = "numberTwo")String numberTwo
+	  ) throws Exception {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo) ) {
+			throw new Exception();
+		}
+		return ConvertToDouble(numberOne) + ConvertToDouble(numberTwo);
+	}
+
+	private Double ConvertToDouble(String number) {
+		if(number == null)return 0D;
+		String valor = number.replaceAll(",", ".");
+		if(isNumeric(valor)) return Double.parseDouble(valor);
+		return 0D;
+	}
+
+	private boolean isNumeric(String number) {
+		if(number == null)return false;
+		String valor = number.replaceAll(",", ".");
+		return valor.matches("[-+]?[0-9]*\\.?[0-9]+");
+		
+	}
+	
+}
---
+++ 05.firstStepsInJavaWithSpringBoot/rest-with-spring-boot-java-erutio/src/main/java/br/com/erudio/exceptions/ExceptionsResponce.java
@@ -0,0 +1,34 @@
+package br.com.erudio.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionsResponce implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date Timestamp;
+	private String message;
+	private String details;
+	
+	public ExceptionsResponce(Date timestamp, String message, String details) {
+		Timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public Date getTimestamp() {
+		return Timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	
+	
+}
