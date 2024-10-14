9c637fe0b4feeb8b4b1ff7bfc83621884cd1e1ea
Joao Pedro <jpmbofc@gmail.com>
2024-10-14 00:38:41 -0300

    [ParamentersAndExceptions] acrescentando subtracao,divisao,multiplicacao,media,raiz

--- 05.firstStepsInJavaWithSpringBoot/rest-with-spring-boot-java-erutio/src/main/java/br/com/erudio/MathController.java
+++ 05.firstStepsInJavaWithSpringBoot/rest-with-spring-boot-java-erutio/src/main/java/br/com/erudio/MathController.java
@@ -25,6 +25,63 @@ public class MathController {
 		}
 		return ConvertToDouble(numberOne) + ConvertToDouble(numberTwo);
 	}
+	
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sub(
+		@PathVariable(value ="numberOne") String numberOne,
+	    @PathVariable(value ="numberTwo") String numberTwo) {
+			if (!isNumeric(numberOne) || !isNumeric(numberTwo) ) {
+				throw new UnsupportedMathOperationException("Please set a numeric value");
+			}
+
+			return ConvertToDouble(numberOne) - ConvertToDouble(numberTwo);
+	}
+
+	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double mult(
+		@PathVariable(value ="numberOne") String numberOne,
+	    @PathVariable(value ="numberTwo") String numberTwo) {
+			if (!isNumeric(numberOne) || !isNumeric(numberTwo) ) {
+				throw new UnsupportedMathOperationException("Please set a numeric value");
+			}
+
+			return ConvertToDouble(numberOne) * ConvertToDouble(numberTwo);
+	}
+
+	@RequestMapping(value = "/div/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double div(
+		@PathVariable(value ="numberOne") String numberOne,
+	    @PathVariable(value ="numberTwo") String numberTwo) {
+			if (!isNumeric(numberOne) || !isNumeric(numberTwo) ) {
+				throw new UnsupportedMathOperationException("Please set a numeric value");
+			}
+
+			return ConvertToDouble(numberOne) / ConvertToDouble(numberTwo);
+	}
+
+	@RequestMapping(value = "/med/{numberOne}/{numberTwo}/{numberTree}", method=RequestMethod.GET)
+	public Double med(
+		@PathVariable(value ="numberOne") String numberOne,
+	    @PathVariable(value ="numberTwo") String numberTwo,
+		@PathVariable(value = "numberTree") String numberTree) {
+			if (!isNumeric(numberOne) || !isNumeric(numberTwo) || !isNumeric(numberTree) ) {
+				throw new UnsupportedMathOperationException("Please set a numeric value");
+			}
+
+			return ((ConvertToDouble(numberOne) + ConvertToDouble(numberTwo) + ConvertToDouble(numberTree))/3);
+	}
+
+	@RequestMapping(value = "/raiz/{numberOne}", method=RequestMethod.GET)
+	public Double raiz(
+		@PathVariable(value ="numberOne") String numberOne) {
+			if (!isNumeric(numberOne) ) {
+				throw new UnsupportedMathOperationException("Please set a numeric value");
+			}
+
+			Double raiz = Math.sqrt(ConvertToDouble(numberOne));
+
+			return raiz;
+	}
 
 	private Double ConvertToDouble(String number) {
 		if(number == null)return 0D;
